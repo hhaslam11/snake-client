ad599c8098bfa8927b3169f96394a3543a2c9e8a
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-27 18:35:09 +0000

    implemented messages and auto moving

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+let clearValue;
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -11,7 +14,34 @@ const setupInput = function() {
 
 const handleUserInput = function(data) {
   if (data === '\u0003') process.exit();
-  if (data === 'a') console.log('woah dude');
+
+  if (data.toLowerCase() === 'w') {
+    clearInterval(clearValue);
+    clearValue = setInterval(() => {
+      connection.write('Move: up');
+    }, 80);
+  }
+  if (data.toLowerCase() === 'a') {
+    clearInterval(clearValue);
+    clearValue = setInterval(() => {
+      connection.write('Move: left');
+    }, 50);
+  }
+  if (data.toLowerCase() === 's') {
+    clearInterval(clearValue);
+    clearValue = setInterval(() => {
+      connection.write('Move: down');
+    }, 80);
+  }
+  if (data.toLowerCase() === 'd') {
+    clearInterval(clearValue);
+    clearValue = setInterval(() => {
+      connection.write('Move: right');
+    }, 50);
+  }
+  if (data.toLowerCase() === '1') connection.write('Say: :)');
+  if (data.toLowerCase() === '2') connection.write('Say: :(');
+  if (data.toLowerCase() === '3') connection.write('Say: >_>');
 };
 
 module.exports = {
