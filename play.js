b3cb8a3642028526cbbdf866025365c38e9a77dc
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-27 17:39:29 +0000

    moved input code to input.js

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const connect = require('./client');
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-
-  stdin.on('data', key => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
-
-const handleUserInput = function(data) {
-  if (data === '\u0003') process.exit();
-  if (data === 'a') console.log('woah dude');
-};
-
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-setupInput();
 connect();
+setupInput();
